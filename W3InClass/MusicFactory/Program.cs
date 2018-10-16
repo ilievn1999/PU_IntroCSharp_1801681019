04955891ad574d493f4d357fa2acfc4086c71495
ilievn1999 <ilievn1999@gmail.com>
2018-10-16 15:49:37 +0300

    initial commit music factory.

--- W3InClass/MusicFactory/Program.cs
+++ W3InClass/MusicFactory/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,35 @@ namespace MusicFactory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Play some music...");
+            Console.WriteLine("Do you want to play some music?");
+            string answer = Console.ReadLine();
+
+            if (answer=="Yes")
+            {
+                using (SoundPlayer player = new SoundPlayer(@"C:\Users\fmi\Desktop\W3\PU_IntroCSharp_1801681019\W3InClass\MusicFactory\Music\574241539693511.wav"))
+                {
+                    Console.WriteLine("Now playing...");
+                    player.PlaySync();
+                    while (true)
+                    {
+                        
+                    }
+                }
+            }
+            else if(answer=="Fuck")
+            {
+                Console.WriteLine("Yes, fuck you too :) ");
+            }
+            else if (answer == "No")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong input!");
+            }
+
+
         }
     }
 }
